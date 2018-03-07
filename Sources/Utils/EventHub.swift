698c2a974438d74a67cc3a13a22391cfbbdbc3ea
almog <almog@guggy.com>
2018-03-07 18:17:30 +0200

    insert meme button

--- Sources/Utils/EventHub.swift
+++ Sources/Utils/EventHub.swift
@@ -1,17 +1,19 @@
 import Foundation
 
 class EventHub {
-
-  typealias Action = () -> Void
-
-  static let shared = EventHub()
-
-  // MARK: Initialization
-
-  init() {}
-
-  var close: Action?
-  var doneWithImages: Action?
-  var doneWithVideos: Action?
-  var stackViewTouched: Action?
+    
+    typealias Action = () -> Void
+    
+    static let shared = EventHub()
+    
+    // MARK: Initialization
+    
+    init() {}
+    
+    var close: Action?
+    var doneWithImages: Action?
+    var createMeme: Action?
+    var doneWithVideos: Action?
+    var stackViewTouched: Action?
 }
+
